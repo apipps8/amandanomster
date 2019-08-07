e01eb5925bfabc89971bb3cabec72ab1a6bd484e
Amanda Pippins <apipps8@gmail.com>
2019-08-07 00:43:59 +0000

    Got models set up

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,2 @@
 class PlacesController < ApplicationController
-	def index
-  	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_033633) do
+ActiveRecord::Schema.define(version: 2019_08_06_032233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "places", force: :cascade do |t|
+  create_table "places", id: :serial, force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "address"
