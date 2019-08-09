82eaddcb2f956514dd33fe215c61ab4c14785da5
Amanda Pippins <apipps8@gmail.com>
2019-08-09 02:55:13 +0000

    added places to nomster

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_032233) do
+ActiveRecord::Schema.define(version: 2019_08_08_195231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
