a7a22d7acf3cd811b384b26d7bc4629dd925328e
Amanda Pippins <apipps8@gmail.com>
2019-08-18 20:59:27 +0000

    added setup to divise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { 'nomster-amanda-pippins.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'nomster-amanda-pippins.herokuapp.com' }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
