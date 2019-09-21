184e35bf62873f8b51a06b63b24fdf1b5252f3c4
Amanda Pippins <apipps8@gmail.com>
2019-09-21 02:42:23 +0000

    adding user page

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2019_09_06_032602) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
